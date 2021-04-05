d0e2ba4afaceb1667f14d202d22b78f68929a743
shizhiping999 <shizhiping2006@163.com>
2021-04-05 09:25:36 +0800

    1003

--- foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public Object hello(){
-        return "123_456_789_000_111_444";
+        return "123_456_789_000_111_444_555";
     }
 }
